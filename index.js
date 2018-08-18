256650dffe082734a5e7393d3c1df97f3710e01c
Arnaldas Augutis <info@untitled.lt>
2018-08-18 22:27:55 +0300

    added promises by referenceId

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const ipc = require('node-ipc');
 
 
 class ipcClient extends EventEmitter {
-    constructor({ name }) {
+    constructor({name}) {
         super();
         if (!name) {
             throw 'No client name provided!';
         }
         console.info(`Creating ipc client with ID ${name}`);
         this.name = name;
+        this.promises = [];
         ipc.config.id = name;
         ipc.config.silent = true;
         this._connect();
@@ -19,19 +20,60 @@ class ipcClient extends EventEmitter {
         ipc.connectTo(
             'master',
             () => {
-                ipc.of.master.on(
-                    this.name,
-                    data => this.emit('message', data)
-                );
+                ipc.of.master.on(this.name, this._onMessage.bind(this));
             }
         );
+    }
+
+    _onMessage(data) {
+        const {payload = {}} = data;
+        const {requestId} = payload;
+
+        if (requestId) {
+            this._resolvePromise({requestId, data});
+
+        } else {
+            this.emit('message', data);
+        }
+    }
+
+    _resolvePromise({requestId, data}) {
+        console.log(`Got data to requestId: ${requestId}`)
+        const promiseObject = this.promises.find(item => item.requestId === requestId);
+        if (promiseObject) {
+            this._removePromise(promiseObject)
+            clearTimeout(promiseObject.rejectTimer);
+            promiseObject.resolve(data);
+        }
+    }
+
+    _removePromise(ref) {
+        const index = this.promises.indexOf(ref);
+        if (index !== -1) {
+            console.log(`Remove promise with requestId: ${ref.requestId}`);
+            this.promises.splice(index, 1);
+        }
+    }
+
+    addPromise({requestId, resolve, reject, timeout = 2000}) {
+        console.log(`Add promise with requestId: ${requestId}`);
+        let promiseObject;
+        promiseObject = {
+            requestId,
+            resolve,
+            rejectTimer: setTimeout(() => {
+                this._removePromise(promiseObject);
+            }, timeout)
+        };
+
+        this.promises.push(promiseObject);
     }
 
     send(topic = '', payload) {
         if (typeof ipc.of.master !== 'undefined') {
             ipc.of.master.emit(
                 this.name,
-                { topic, payload }
+                {topic, payload}
             )
         } else {
             console.error('Connection not ready yet!');
